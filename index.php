5326f8fd63a695bfd2a5efc5f1f374bac38c652b
MuhammedSami <muhammedsami6@gmail.com>
2021-10-28 23:25:48 +0300

    require_once autoload,
    Create new Instance of Application
    Create the first route with get method.
    
    run the app.

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+require_once __DIR__.'/vendor/autoload.php';
 
-echo "Hello World";
+use App\Core\Application;
+
+$app = new Application();
+
+$app->router->get('/', function (){
+    echo "Hello World";
+});
+
+$app->run();
