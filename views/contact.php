a340bb199007503ff9d908b6999faee0baede542
MuhammedSami <muhammedsami6@gmail.com>
2021-10-29 23:40:00 +0300

    Add views for testing purpose.

---
+++ views/contact.php
@@ -0,0 +1,13 @@
+<h1>Contact</h1>
+
+<form method="post">
+  <div class="mb-3">
+    <label>Subject</label>
+    <input type="text" name="subject" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label>Mesagge</label>
+    <input type="text" name="email" class="form-control">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
