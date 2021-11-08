52421b64f667e1725864dad0c0c9ab9de6ac77cb
MuhammedSami <muhammedsami6@gmail.com>
2021-11-09 00:06:46 +0300

    Remove Command functionalities from Console creation level.

--- Core/Console.php
+++ Core/Console.php
@@ -8,47 +8,16 @@ namespace App\Core;
  */
 class Console
 {
-    protected ConsoleFormatter $formatter;
-
-    /**
-     * @return \App\Core\ConsoleFormatter
-     */
-    public function getFormatter(): \App\Core\ConsoleFormatter
-    {
-        return $this->formatter;
-    }
-
-    protected array $registry;
+    protected Command $registry;
 
     public function __construct()
     {
-        $this->formatter = new ConsoleFormatter();
+        $this->registry = new Command();
+        $this->registry->registerPredefinedCommands();
     }
 
-    public function runCommand(array $argv)
+    public function make(array $argv = [])
     {
-        $commandName = "help";
-
-        if (isset($argv[1])){
-            $commandName = $argv[1];
-        }
-
-        $command = $this->getCommand($commandName);
-        if (!$command){
-            $this->formatter->display("ERROR: Command $command not found!");
-            return;
-        }
-
-        call_user_func($command, $argv);
-    }
-
-    public function registerCommand($name, $callback)
-    {
-        $this->registry[$name] = $callback;
-    }
-
-    public function getCommand($command)
-    {
-        return $this->registry[$command] ??  null;
+        $this->registry->run($argv);
     }
 }
