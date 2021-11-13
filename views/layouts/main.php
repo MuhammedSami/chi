745b2d0e43820f5e10172818ef375a0687caa102
MuhammedSami <muhammedsami6@gmail.com>
2021-11-13 22:33:51 +0300

    feat(views): Add auth layout, login and register views.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -30,6 +30,16 @@
                     </li>
 
                 </ul>
+
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/register">Register</a>
+                    </li>
+
+                </ul>
                 <form class="d-flex">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Search</button>
