746358b622f8ee3495a5c83a4019e6b65baaa01c
MuhammedSami <muhammedsami6@gmail.com>
2021-11-19 23:48:26 +0300

    feat!: add custom validation to the model.

--- Core/Model.php
+++ Core/Model.php
@@ -14,6 +14,8 @@ abstract class Model
     const RULE_MAX = 'max';
     const RULE_MATCH = 'match';
 
+    public array $errors;
+
     public function loadData(array $data)
     {
         foreach ($data as $key => $value) {
@@ -25,13 +27,59 @@ abstract class Model
 
     public function validate()
     {
-       /* foreach ($this->rules() as $attribute => $rules){
-            $valeu = $this->{$attribute};
-            foreach ($rules as $rule){
+        foreach ($this->rules() as $attribute => $rules) {
+            $value = $this->{$attribute};
+            foreach ($rules as $rule) {
+                $ruleName = $rule;
+                if (! is_string($ruleName)) {
+                    $ruleName = $rule[0];
+                }
 
+                if ($ruleName === self::RULE_REQUIRED && ! $value) {
+                    $this->addError($attribute, self::RULE_REQUIRED);
+                }
+
+                if ($ruleName === self::RULE_EMAIL && ! filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                    $this->addError($attribute, self::RULE_EMAIL);
+                }
+
+                if ($ruleName === self::RULE_MIN && strlen($value) < $rule['min']) {
+                    $this->addError($attribute, self::RULE_MIN, $rule);
+                }
+
+                if ($ruleName === self::RULE_MAX && strlen($value) > $rule['max']) {
+                    $this->addError($attribute, self::RULE_MAX, $rule);
+                }
+
+                if ($ruleName === self::RULE_MATCH && $value !== $this->{$rule['match']}) {
+                    $this->addError($attribute, self::RULE_MATCH, $rule);
+                }
             }
-        }*/
+        }
+
+        return empty($this->errors);
     }
 
     abstract public function rules(): array;
+
+    public function addError(string $attribute, string $rule, $params = [])
+    {
+        $message = $this->errorMessages()[$rule] ?? '';
+        foreach ($params as $key => $value) {
+            $message = str_replace("{{$key}}", $value, $message);
+        }
+        $message = str_replace("{attribute}", $attribute, $message);
+        $this->errors[$attribute][] = $message;
+    }
+
+    public function errorMessages()
+    {
+        return [
+            self::RULE_REQUIRED => '{attribute} field is required!',
+            self::RULE_EMAIL    => '{attribute} field must be valid email address',
+            self::RULE_MIN      => 'Min length of {attribute} must be {min}',
+            self::RULE_MAX      => 'Max length of {attribute} must be {MAX}',
+            self::RULE_MATCH    => 'attribute must be the same as {match}',
+        ];
+    }
 }
